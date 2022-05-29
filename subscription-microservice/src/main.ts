0dace67f66b22675c4f6c771961090020d40e46c
CherifYasmine <yasmine.chirif@insat.u-carthage.tn>
2022-05-29 16:57:17 +0100

    api

--- subscription-microservice/src/main.ts
+++ subscription-microservice/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: { port: 6379 },
+  });
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: { port: 3005 },
+  });
+  await app.startAllMicroservices();
+  await app.listen(3005);
 }
 bootstrap();
